fc19a2eef2d6b2eeb3061b00b6a79970afc2792b
Steve Tomi <steveola23@gmail.com>
2025-04-13 16:25:41 +0100

    Add relayer

---
+++ script/Relayer.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "forge-std/Vm.sol";
+import "../src/smart-wallet/Relayer.sol";
+
+contract DeployScript is Script {
+    function run() external {
+        uint256 OWNER_PK = vm.envUint("OWNER");
+
+        uint256 SMARTWALLET_ADMIN_PK = vm.envUint("SMARTWALLET_ADMIN");
+        address SMARTWALLET_ADMIN = vm.addr(SMARTWALLET_ADMIN_PK);
+
+        // Start broadcasting transactions using the Owner private key
+        vm.startBroadcast(OWNER_PK);
+
+        Relayer deployer = new Relayer(
+            SMARTWALLET_ADMIN
+        );
+
+        console.log("Deployed Relayer at:", address(deployer));
+        vm.stopBroadcast();
+
+    }
+}
---
+++ src/smart-wallet/Relayer.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "../lib/SafeTransferLib.sol";
+import "../lib/ERC20.sol";
+import "../utils/Owner.sol";
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address who) external returns (uint);
+}
+
+
+contract Relayer is Owner {
+
+    event TransactionSent(address indexed from, address indexed to, uint256 amount, address indexed token);
+
+    mapping(address => bool) public relayers;
+
+    constructor(address first_relayer) Owner(first_relayer) {
+        relayers[first_relayer] = true;
+    }
+
+    modifier onlyRelayer() {
+        require(relayers[msg.sender], "Only whitelisted relayers can call this function");
+        _;
+    }
+
+    function addRelayer(address _relayer) external {
+        onlyOwner();
+        relayers[_relayer] = true;
+    }
+
+    function removeRelayer(address _relayer) external {
+        onlyOwner();
+        relayers[_relayer] = false;
+    }
+
+    function sendETH(address from, address to, uint256 amount) private {
+        require(address(from).balance >= amount, "Sender has insufficient balance");
+        SafeTransferLib.safeTransferETH(to, amount);
+    }
+
+    function sendToken(address to, uint256 amount, address token) private {
+        require(IERC20(token).balanceOf(to) >= amount, "Sender has insufficient balance");
+        SafeTransferLib.safeTransfer(ERC20(token), to, amount);
+    }
+
+    function send(address from, address to, uint256 amount, address token) external onlyRelayer {
+        if (token == address(0)) {
+            sendETH(from, to, amount); 
+        } else {
+            sendToken(to, amount, token); 
+        }
+        emit TransactionSent(from, to, amount, token);
+    }
+
+}
